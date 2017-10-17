ca1714d624633620c26c8dafdb53aa49600af5ed
John Arley <johnarleycano@hotmail.com>
2017-10-17 10:28:21 -0500

    Eliminada carpeta viewsx

--- application/viewsx/includes/footer.php
+++
@@ -1,5 +0,0 @@
-<!DOCTYPE html>
-        <div align="center" id="footer">
-            <p>&copy;Todos los derechos reservados - <?php echo anchor_popup('http://www.devimed.com.co/','<b>Devimed S.A</b>'); ?></p>
-            <div class="clear"></div>
-        </div>
