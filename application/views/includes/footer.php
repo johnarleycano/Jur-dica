59d5ce61736dad7fa74d17d62557a75530872632
John Arley <johnarleycano@hotmail.com>
2018-08-02 14:16:19 -0500

    style(general): mostrar versión en pantalla

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,5 +1,16 @@
-<!DOCTYPE html>
-        <div align="center" id="footer">
-            <p>&copy;Todos los derechos reservados - <?php echo anchor_popup('http://www.devimed.com.co/','<b>Devimed S.A</b>'); ?></p>
-            <div class="clear"></div>
-        </div>
+<div align="center" id="footer">
+    <p>&copy;Todos los derechos reservados - <?php echo anchor_popup('http://www.devimed.com.co/','<b>Devimed S.A.</b>'); ?> | <i>Versión <b><?php echo version(); ?></b></p>
+    <div class="clear"></div>
+
+    <?php
+	function version()
+	{
+	    foreach(array_reverse(glob('.git/refs/tags/*')) as $archivo) {
+	        $contents = file_get_contents($archivo);
+
+	        return basename($archivo);
+	        exit();
+	    }
+	}
+	?>
+</div>
